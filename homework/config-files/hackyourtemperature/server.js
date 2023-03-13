5483e45ffb28ee5184e4936e5ff05c8fc094f417
Abdullah Omer ALIMOGLU <aoalimoglu@gmail.com>
2023-03-13 14:28:24 +0100

    installed the necessary modules and added the external API

--- homework/config-files/hackyourtemperature/server.js
+++ homework/config-files/hackyourtemperature/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import exphbs from "express-handlebars";
 import fetch from "node-fetch";
+import { API_KEY } from "./sources/keys.js";
 
 const app = express();
 const port = 3000;
@@ -13,7 +14,30 @@ app.use(express.json());
 
 app.post("/weather", (req, res) => {
   const cityName = req.body.cityName;
-  res.send(cityName);
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.cod === "404") {
+        res.send({ weatherText: "City is not found!" });
+      } else {
+        const temperature = data.main.temp;
+        const celsius = Math.round(temperature - 273.15);
+        const weatherText = `The temperature in ${cityName} is ${celsius} degrees Celsius.`;
+        res.send({ weatherText });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send({ weatherText: "Error occurred while fetching weather data." });
+    });
+});
+
+// send response the 404 error message
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry, we cannot find the city!");
 });
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
---
+++ homework/config-files/hackyourtemperature/sources/keys.js
@@ -0,0 +1,1 @@
+export const API_KEY = "6ec1b8a1d978c4c857830b7329dcc3d2";
